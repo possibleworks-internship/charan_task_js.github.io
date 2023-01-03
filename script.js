cbfbed81d607ab24baa2cb752576e2d92d392713
Saicharanbhogi <saicharanbhogi2001@gmail.com>
2023-01-03 10:07:50 +0530

    naming conventions modified

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var arr = [];
+var usersData = [];
 let newdata = {};
-const context = document.querySelector(".main");
+const containerBlock = document.querySelector(".main");
 
 //form inputs
 let name1 = document.querySelector("#name");
@@ -37,7 +37,7 @@ const block = function (maindata, index = 0) {
             }'>DELETE</button>
             </div>
         <div>`;
-    context.insertAdjacentHTML("beforeend", html);
+    containerBlock.insertAdjacentHTML("beforeend", html);
   });
   deleteButton();
   updatebutton();
@@ -49,14 +49,14 @@ const fetchData = async function () {
   const data = await response.json();
   window.localStorage.setItem("usersdata", JSON.stringify(data));
   let webdata = window.localStorage.getItem("usersdata");
-  arr.push(JSON.parse(webdata));
-  let maindata = arr.flat(1);
+  usersData.push(JSON.parse(webdata));
+  let maindata = usersData.flat(1);
   console.log(maindata);
   block(maindata);
 };
 if (window.localStorage.getItem("usersdata")) {
-  arr = JSON.parse(window.localStorage.getItem("usersdata"));
-  block(arr);
+  usersData = JSON.parse(window.localStorage.getItem("usersdata"));
+  block(usersData);
 } else {
   fetchData();
 }
@@ -80,13 +80,13 @@ formdata.addEventListener("submit", function (e) {
   })
     .then((response) => response.json())
     .then((data) => {
-      arr = JSON.parse(window.localStorage.getItem("usersdata"));
-      const i = arr.length;
-      console.log(arr);
-      arr.push(data);
-      window.localStorage.setItem("usersdata", JSON.stringify(arr));
-      let [newone] = arr.slice(-1);
-      block([newone], i);
+      usersData = JSON.parse(window.localStorage.getItem("usersdata"));
+      const i = usersData.length;
+      console.log(usersData);
+      usersData.push(data);
+      window.localStorage.setItem("usersdata", JSON.stringify(usersData));
+      let [newAddeddata] = usersData.slice(-1);
+      block([newAddeddata], i);
     });
   name1.value = username.value = email.value = number.value = "";
 });
@@ -94,28 +94,27 @@ formdata.addEventListener("submit", function (e) {
 //deleting the data
 
 function deleteButton() {
-  var del_btn = document.getElementsByClassName("delete");
-  [...del_btn].forEach((ele) => {
+  var delBtnlist = document.getElementsByClassName("delete");
+  [...delBtnlist].forEach((ele) => {
     ele.addEventListener("click", function (e) {
       e.preventDefault();
-      let del_class = ele.className;
-      let del_sld = del_class.slice(7);
-      console.log(del_sld);
-      let del_id = del_sld.match(/(\d+)/)[0];
-      console.log(del_id);
+      let delBtnClasses = ele.className;
+      let sliceData = delBtnClasses.slice(7);
+      console.log(sliceData);
+      let delId = sliceData.match(/(\d+)/)[0];
+      console.log(delId);
       if (confirm("Want to delete?")) {
-        fetch(`https://jsonplaceholder.typicode.com/users/${del_id}`, {
+        fetch(`https://jsonplaceholder.typicode.com/users/${delId}`, {
           method: "DELETE",
         })
           .then((response) => response.json())
           .then((data) => {
-            let del = document.getElementById(`mainrow--${del_id}`);
+            let del = document.getElementById(`mainrow--${delId}`);
             console.log(del);
-            context.removeChild(del);
-            arr = JSON.parse(window.localStorage.getItem("usersdata"));
-            arr.splice(del_id, 1);
-            console.log(del_id-1);
-            window.localStorage.setItem("usersdata", JSON.stringify(arr));
+            containerBlock.removeChild(del);
+            usersData = JSON.parse(window.localStorage.getItem("usersdata"));
+            usersData.splice(delId, 1);
+            window.localStorage.setItem("usersdata", JSON.stringify(usersData));
           });
       }
     });
@@ -124,32 +123,32 @@ function deleteButton() {
 
 //updating data
 function updatebutton() {
-  var update_btn = document.getElementsByClassName("update");
-  [...update_btn].forEach((ele) => {
+  var updateBtnlist = document.getElementsByClassName("update");
+  [...updateBtnlist].forEach((ele) => {
     ele.addEventListener("click", function (e) {
-      let upd = ele.className;
-      let upd_sld = upd.slice(8);
-      let upd_id = upd_sld.match(/(\d+)/)[0];
-      var add_input = document.createElement("input");
-      add_input.type = "text";
-      add_input.placeholder = "Update your name here";
-      const add = document.getElementById(`input_add--${upd_id}`);
-      add.appendChild(add_input);
+      let updBtnClasses = ele.className;
+      let sliceData = updBtnClasses.slice(8);
+      let updId = sliceData.match(/(\d+)/)[0];
+      var addInput = document.createElement("input");
+      addInput.type = "text";
+      addInput.placeholder = "Update your name here";
+      const inputDiv = document.getElementById(`input_add--${updId}`);
+      inputDiv.appendChild(addInput);
       ele.classList.add("hidden");
-      let add_button_save = document.createElement("button");
-      add_button_save.textContent = "SAVE";
-      add_button_save.classList.add(`btn_save--${upd_id}`);
-      const add_bt = document.getElementById(`btn_add--${upd_id}`);
-      const name_input = document.getElementById(`add--${upd_id}`);
-      add_bt.appendChild(add_button_save);
-      add_button_save.addEventListener("click", function (e) {
+      let addBtnsave = document.createElement("button");
+      addBtnsave.textContent = "SAVE";
+      addBtnsave.classList.add(`btn_save--${updId}`);
+      const updateDiv = document.getElementById(`btn_add--${updId}`);
+      const nameFieldvalue = document.getElementById(`add--${updId}`);
+      updateDiv.appendChild(addBtnsave);
+      addBtnsave.addEventListener("click", function (e) {
         e.preventDefault();
-        let add_new = add_input.value;
-        if (add_new === "") return;
-        fetch(`https://jsonplaceholder.typicode.com/users/${upd_id}`, {
+        let newInputname = addInput.value;
+        if (newInputname === "") return;
+        fetch(`https://jsonplaceholder.typicode.com/users/${updId}`, {
           method: "PATCH",
           body: JSON.stringify({
-            name: add_new,
+            name: newInputname,
           }),
           headers: {
             "Content-type": "application/json; charset=UTF-8",
@@ -157,14 +156,14 @@ function updatebutton() {
         })
           .then((response) => response.json())
           .then((data) => {
-            name_input.textContent = "";
-            name_input.textContent = data.name;
+            nameFieldvalue.textContent = "";
+            nameFieldvalue.textContent = data.name;
             ele.classList.remove("hidden");
-            add_button_save.classList.add("hidden");
-            add_input.classList.add("hidden");
-            arr = JSON.parse(window.localStorage.getItem("usersdata"));
-            arr[upd_id].name = data.name;
-            window.localStorage.setItem("usersdata", JSON.stringify(arr));
+            addBtnsave.classList.add("hidden");
+            addInput.classList.add("hidden");
+            usersData = JSON.parse(window.localStorage.getItem("usersdata"));
+            usersData[updId].name = data.name;
+            window.localStorage.setItem("usersdata", JSON.stringify(usersData));
           });
       });
     });
@@ -172,30 +171,30 @@ function updatebutton() {
 }
 
 //searching for data
-let c=0;
+let check=0;
 var value;
-const remove=function(arr){
-  arr.forEach((_, i) => {
+const remove=function(removeData){
+  removeData.forEach((_, i) => {
     let cls = document.getElementById(`mainrow--${i}`);
-    context.removeChild(cls);
+    containerBlock.removeChild(cls);
   });
 }
 document
   .querySelector(".search_button")
   .addEventListener("click", function (e) {
     e.preventDefault();
-    let search_data = document.querySelector(".search").value.toLowerCase();
-    console.log(search_data);
-    if (search_data === "") {
+    let searchInputdata = document.querySelector(".search").value.toLowerCase();
+    console.log(searchInputdata);
+    if (searchInputdata === "") {
       alert("No data found or please enter valid data")
     }
-    if(c==0){
-      remove(arr)
-      c+=1
+    if(check==0){
+      remove(usersData)
+      check+=1
     }
     else{
-      remove(value)
+      remove(filteredData)
     }
-    value = arr.filter((el) => el.name.toLowerCase().includes(search_data));
-    block(value);
+    filteredData = usersData.filter((el) => el.name.toLowerCase().includes(searchInputdata));
+    block(filteredData);
   });
